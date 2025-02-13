8c622b539a020ef1f29b4c20c517c4814c7a4739
Hitesh <hiteshna@atrihub.io>
2025-02-13 15:45:16 -0800

    feat(refactor): Chapter 4 Return html page(success) [ICTRAINING-1827]

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
 
+# Refactoring the functional test
+from django.urls import reverse
+
 # Create your tests here.
 
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/')
+        # response = self.client.get('/lists/')
+
+        # Refactor
+        response = self.client.get(reverse('view_list'))
         self.assertTemplateUsed(response, 'list.html')
