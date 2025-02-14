5cc1316391eb72df955d19d2709f8ab454456a8e
Hitesh <hiteshna@atrihub.io>
2025-02-13 16:42:28 -0800

    feat(fix): Refactored code in Testing and check saved data in database [ICTRAINING-1828]

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class List(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -19,4 +19,16 @@ def home(request):
 def view_list(request):
     new_list = ListForm()
     context = {'form': new_list}
+    if request.method == 'POST':
+        new_list = ListForm(request.POST)
+    if new_list.is_valid():
+        
+        list_name = new_list.cleaned_data['name']
+        if not List.objects.filter(name=list_name).exists():
+            # Creates an instance and uncouples the dictionary
+            created_list = List.objects.create(**new_list.cleaned_data)
+            # trys to save the instance to the database( works incrementally)
+            context['created_list_id'] = created_list.id
+            return redirect('/')
+        
     return render(request, 'list.html',context)
